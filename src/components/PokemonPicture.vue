951ff50178121eaa5e6b3ae258a49c28bbe9b647
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2022-02-08 13:29:50 -0600

    Curso Vue | Diseño de los componentes

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,5 +1,14 @@
 <template>
-  <h3>Pokemon picture</h3>
+<div class="pokemon-container">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+        class="hidden-pokemon"
+        alt="pokemon">
+
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+        class="fade-in"
+        alt="pokemon">
+</div>
+  
 </template>
 
 <script>
@@ -8,6 +17,21 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.pokemon-container {
+    height: 200px;
+}
+img {
+    height: 200px;
+    position: absolute;
+    right: 32%;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
 </style>
